d09a64455cfb971befa2f79ac892b4b229b494d0
noelma <noel-mathieu@hotmail.fr>
2020-04-26 21:18:32 +0200

    Mise à jour pour la version beta1 du CMS.

--- views-news-index.php
+++ views-news-index.php
@@ -1,22 +1,32 @@
 
 <section class="wrapper style3 container special">
     <div class="row">
-        <?php if ($nodes): ?>
-            <?php foreach ($nodes as $node): ?>
-                <?php $node[ 'field' ] = unserialize($node[ 'field' ]); ?>
+        <?php if ($news): ?>
+            <?php foreach ($news as $new): ?>
 
                 <div class="col-6 col-12-narrower">
                     <section>
+                        <a class="image featured" href="#">
+                            <img src="<?php echo $new[ 'field' ][ 'image' ][ 'field_value' ]; ?>" alt="Illustration <?php echo $new[ 'title' ]; ?>">
+                        </a>
                         <header>
-                            <h3><a href="<?php echo $node[ 'link_view' ]; ?>"><?php echo $node[ 'title' ]; ?></a></h3>
+                            <h3><a href="<?php echo $new[ 'link_view' ]; ?>"><?php echo $new[ 'title' ]; ?></a></h3>
                         </header>
-                        <small><?php echo date('F d, Y', $node[ 'created' ]); ?></small>
-                        <p><?php echo $node[ 'field' ][ 'summary' ]; ?></p>
-                        <a href="<?php echo $node[ 'link_view' ]; ?>" class="button">En savoir plus</a>
+                        <small><?php echo date('F d, Y', $new[ 'date_created' ]); ?></small> ~ 
+                        <small><?php echo $new[ 'field' ][ 'reading_time' ][ 'field_value' ] . ' ' . t('minute(s)'); ?></small>
+
+                        <p><?php echo $new[ 'field' ][ 'summary' ][ 'field_display' ]; ?></p>
+                        <a href="<?php echo $new[ 'link_view' ]; ?>" class="button"><?php echo t('Learn more'); ?></a>
                     </section>
                 </div>
             <?php endforeach; ?>
-        <?php else: ?> 
+            <div class="col-md-12">
+                <?php echo $paginate; ?>
+            </div>
+            <div class="col-md-12">
+                <a type="application/rss+xml" href="<?php echo $link_rss; ?>" title="Flux RSS"><i class="fa fa-rss-square" aria-hidden="true"></i></a>
+            </div>
+        <?php else: ?>
 
             <div class="col-12 col-12-narrower">
                 <p><?php echo $default; ?></p>
